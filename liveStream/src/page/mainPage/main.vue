c1d26acc543320af5a5e06f1bf1dbad5406dcc1e
V1zefull <borovlevegor@gmail.com>
2024-05-30 20:03:01 +0300

    qwe

--- liveStream/src/page/mainPage/main.vue
+++ liveStream/src/page/mainPage/main.vue
@@ -1,44 +1,54 @@
 <script>
 import chatComp from '../../components/chatComponent/chat.vue';
 import Follower from '../../components/followerComponent/follower.vue';
+import Player from '../../components/playerComponent/player.vue'
 export default {
-    components:{
+    components: {
+        Player,
         chatComp,
         Follower,
     },
     data() {
-      return {
+        return {
 
-      };
+        };
     },
-  };
+};
 </script>
 
 <template>
     <div class="container">
         <div class="container__main">
-            <div class="container__follower"><Follower/></div>
-            
-            <div class="container__chat"><chatComp/></div>
+            <div class="container__follower">
+                <Follower />
+            </div>
+            <div class="container_player">
+                    <Player class="videoplayer" src="test.mp4" :muted="false" :autoplay="true" :controls="true"
+                        :loop="false" poster="jpg_44-2.jpg"></Player>
+            </div>
+            <div class="container__chat">
+                <chatComp />
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.container{
+.container {
     width: 100%;
     height: 100%;
-    &__main{
+
+    &__main {
         width: 100%;
         height: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
-    &__follower{
+
+    &__follower {
         width: 20em;
         height: 100%;
     }
 }
-
 </style>
